b616cbd38b19e4ffaf278dc0cf2ed3c42d78034e
Shiratic <TommyIsWerid@gmail.com>
2025-09-10 22:51:18 -0500

    Health , Enemy Damage

---
+++ Assets/OvertimeHaunt/Scripts/PlayerHealth.cs
@@ -0,0 +1,21 @@
+using UnityEngine;
+
+public class PlayerHealth : MonoBehaviour
+{
+    public int currentHealth;
+    public int maxHealth;
+
+    public SpriteRenderer playerSr;
+    public PlayerMovement playerMovement;
+
+    public void ChangeHealth(int amount)
+    {
+        currentHealth += amount;
+
+        if(currentHealth <= 0)
+        {
+            playerSr.enabled = false;
+            playerMovement.enabled = false;
+        }
+    }
+}
