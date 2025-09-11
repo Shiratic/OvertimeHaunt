0e061415a4b7f70cb761bedcd4c5b530326117e7
Shiratic <TommyIsWerid@gmail.com>
2025-09-10 22:08:57 -0500

    Sprites, Movement, Camera

---
+++ Assets/OvertimeHaunt/Scripts/PlayerMovement.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    public float speed = 5;
+    public Rigidbody2D rb;
+    
+    
+    
+
+    // Fixed Update is called 50x per frame
+    void FixedUpdate()
+    {
+        float horizontal = Input.GetAxis("Horizontal");
+        float vertical = Input.GetAxis("Vertical");
+
+        rb.linearVelocity = new Vector2(horizontal, vertical) * speed;
+    }
+}
